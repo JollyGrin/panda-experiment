231761a902f190ae8f0b573f19b9b8eb75764293
jollygrin <JollyGrin@protonmail.com>
2024-06-29 13:29:55 +0200

    page experiment

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import { css } from "@/styled-system/css";
+import { styled } from "@/styled-system/jsx";
+
 export default function Home() {
   return (
     <main>
-      <p>hello</p>
+      <div>
+        <div
+          className={css({
+            display: "flex",
+            flexDirection: "column",
+            bg: "red",
+            w: "10rem",
+            h: "10rem",
+          })}
+        >
+          <p>hi</p>
+
+          <p>hi</p>
+        </div>
+        <styled.button bg="blue">this is a button</styled.button>
+      </div>
     </main>
   );
 }
